76eb60fa3ea1f49afa24ae32b6e51edca31e3c16
raniamo8 <raneaxxl@gmail.com>
2024-05-06 20:01:07 +0200

    Initial commit

---
+++ src/js/login.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.querySelector('form');
+
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault(); // Verhindert das Neuladen der Seite
+
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        // Statische Zugangsdaten
+        const credentials = {
+            admin: { username: 'admin@example.com', password: 'testadmin' },
+            user: { username: 'user@example.com', password: 'testuser' }
+        };
+
+        // Prüfung der Anmeldedaten
+        if (email === credentials.admin.username && password === credentials.admin.password) {
+            alert('Admin erfolgreich angemeldet');
+            // Weiterleitung oder spezifische Logik für Admin
+        } else if (email === credentials.user.username && password === credentials.user.password) {
+            alert('User erfolgreich angemeldet');
+            // Weiterleitung oder spezifische Logik für User
+        } else {
+            alert('Falsche Anmeldedaten');
+        }
+    });
+});
